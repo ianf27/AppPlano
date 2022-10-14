ff6a5359de562cdb1d157801cb4de11c21bcc603
20201TIIMI0080 <ianluiz.fracalossi@gmail.com>
2022-10-14 17:12:03 -0300

    14/10 falta colocar id em vários layouts

--- app/src/main/java/fracalossi/ian/appplano/EditarPlanejamentoFragment.java
+++ app/src/main/java/fracalossi/ian/appplano/EditarPlanejamentoFragment.java
@@ -2,11 +2,14 @@ package fracalossi.ian.appplano;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ScrollView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,5 +63,21 @@ public class EditarPlanejamentoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_editar_planejamento, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //looping para escrever cada slider de definir porcentagem de categoria
+        // esse 3 será substituído pela quantidade de categorias q a pessoa tiver
+        for (int i=0; i<3; i++) {
+            View v = getLayoutInflater().inflate(R.layout.categoria_sliders_item, null);
+
+            //será necesário usar um findviewbyid para alterar os text view do item
+
+            ScrollView sv = view.findViewById(R.id.svCategorias);
+            sv.addView(v);
+        }
     }
 }
